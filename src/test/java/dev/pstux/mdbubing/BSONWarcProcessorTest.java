8452427a2b7546253df3eef9d04f3e15a2547d55
Pierlauro Sciarelli <foss@pstux.dev>
2020-10-27 17:29:35 +0100

    Added mdb homepage for testing purposes

--- src/test/java/dev/pstux/mdbubing/BSONWarcProcessorTest.java
+++ src/test/java/dev/pstux/mdbubing/BSONWarcProcessorTest.java
@@ -5,19 +5,24 @@ import java.net.URL;
 import org.junit.Test;
 
 import it.unimi.di.law.warc.io.UncompressedWarcReader;
+import it.unimi.di.law.warc.io.WarcFormatException;
 import it.unimi.di.law.warc.io.WarcReader;
 import it.unimi.di.law.warc.records.WarcRecord;
 
 public class BSONWarcProcessorTest {
 	private final BSONWarcProcessor processor = BSONWarcProcessor.INSTANCE;
-	
+
 	@Test
 	public void test() throws Exception	{
-		URL url = BSONWarcProcessorTest.class.getResource("/sample.warc");
+		URL url = BSONWarcProcessorTest.class.getResource("/mongodb-homepage.warc");
 		WarcReader reader = new UncompressedWarcReader(url.openStream());
 		WarcRecord record;
-		while((record= reader.read()) != null) {
-			processor.process(record, 0);
+		try {
+			while((record= reader.read()) != null) {
+				processor.process(record, 0);
+			}
+		}catch(WarcFormatException e) {
+			// TODO log error - some WARC record types are currently unsupported
 		}
 	}
 }
