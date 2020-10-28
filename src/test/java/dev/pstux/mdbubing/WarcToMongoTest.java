b7256e2ebb5e1e29364802fd09f575405669fb19
Pierlauro Sciarelli <foss@pstux.dev>
2020-10-28 18:34:11 +0100

    Commented WarcToMongoTest

--- src/test/java/dev/pstux/mdbubing/WarcToMongoTest.java
+++ src/test/java/dev/pstux/mdbubing/WarcToMongoTest.java
@@ -27,6 +27,7 @@ import static org.mockito.Mockito.verify;
 @PrepareForTest(MongoClients.class)
 public class WarcToMongoTest {
 	@Test
+	/** Test that all valid WARC records are inserted in the collection */
 	public void validRecordsAreInsertedInCollection() throws Exception{
 		// Mock client, database and collection objects
 		@SuppressWarnings("unchecked")
@@ -40,8 +41,10 @@ public class WarcToMongoTest {
 		PowerMockito.mockStatic(MongoClients.class);
 		BDDMockito.given(MongoClients.create(Mockito.anyString())).willReturn(mockClient);
 
+		// Invoke main with expected command line syntax
 		WarcToMongo.main(new String[] {"-P", WarcToMongoTest.class.getResource(TEST_WARC_TO_MONGO_PROPERTIES_PATH).getPath()});
 
+		// Verify that valid records are inserted in the collection
 		verify(mockCollection, times(NUM_VALID_TEST_WARC_RECORDS)).insertOne(Mockito.any(Document.class));
 	}
 }
